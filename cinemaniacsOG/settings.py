956cfdab7996cdc51f4df4df1d2cbf16d75011b0
mugisha636 <mugisha134emmy@gmail.com>
2021-09-13 15:48:32 +0200

    change

--- cinemaniacsOG/settings.py
+++ cinemaniacsOG/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os.path
 
 from pathlib import Path
 
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms'
 ]
 
-MIDDLEWARE = {
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -51,7 +52,7 @@ MIDDLEWARE = {
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-}
+]
 
 ROOT_URLCONF = 'cinemaniacsOG.urls'
 
@@ -124,15 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticFILES')
-
-
-
 STATIC_URL = '/static/'
 
 # add this line inorder to tell django where all our static folders are
 # located
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     'static'
 ]
