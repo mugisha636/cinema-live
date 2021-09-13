9efce7f08bf4ee8634f594120d738cb85982dda6
mugisha636 <mugisha134emmy@gmail.com>
2021-09-13 15:21:05 +0200

    allow static file to road

--- cinemaniacsOG/settings.py
+++ cinemaniacsOG/settings.py
@@ -42,15 +42,16 @@ INSTALLED_APPS = [
     'crispy_forms'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = {
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+}
 
 ROOT_URLCONF = 'cinemaniacsOG.urls'
 
@@ -122,6 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticFILES')
+
+
 
 STATIC_URL = '/static/'
 
